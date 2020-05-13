266f4f680f319373a25c99ec96f707a274dc689f
catherinemcgovern <catherine.l.mcgovern@gmail.com>
2020-05-12 21:32:06 -0500

    first commit

---
+++ Chapter2_Variables_Data_Types/Exercise1.py
@@ -0,0 +1,14 @@
+#This exercise takes a first and last name from a user and then puts them together in a new variable
+
+#First I prompt the user to start the "Hello Machine"
+start = input("Welcome to the hello machine, do you want to enter a name? Please enter Y or N")
+#I start a while loop to begin taking name input
+#if start == "N":
+while start == "Y":
+    #while True:
+    first_name = input("Please enter the first name")
+    last_name = input("please enter your second name")
+    full_name = first_name + " " + last_name
+
+
+
